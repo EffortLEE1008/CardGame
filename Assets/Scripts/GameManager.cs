e7796e403e62a9c2545b334eb4a7ca64dc019219
LazyLEE1008 <66935871+LazyLEE1008@users.noreply.github.com>
2023-04-13 01:22:19 +0900

    almost_doon

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    Select_card[] card;
 
-    
+    [SerializeField]
+    Sprite[] card_image;
+
+    List<int> randnum_lst = new List<int>(); 
+          
     
     void Start()
     {
-        
+        UnduplicateRandom(randnum_lst);
+
+        SpriteRenderer sp_card1 = card[0].GetComponent<SpriteRenderer>();
+        sp_card1.sprite = card_image[randnum_lst[0]];
+
+        SpriteRenderer sp_card2 = card[1].GetComponent<SpriteRenderer>();
+        sp_card2.sprite = card_image[randnum_lst[1]];
+
+        SpriteRenderer sp_card3 = card[2].GetComponent<SpriteRenderer>();
+        sp_card3.sprite = card_image[randnum_lst[2]];
+
+        SpriteRenderer sp_card4 = card[3].GetComponent<SpriteRenderer>();
+        sp_card4.sprite = card_image[randnum_lst[3]];
+
+        SpriteRenderer sp_card5 = card[4].GetComponent<SpriteRenderer>();
+        sp_card5.sprite = card_image[randnum_lst[4]];
+
+        Debug.Log(sp_card1.sprite.name.GetType());
+        //card[0].my_spriteRendere.sprite = card[0].my_sprite[3];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
+
+    void UnduplicateRandom(List<int> tmp_lst)
+    {
+        int tmp_random = Random.Range(0, 32);
+
+        for(int i=0; i < 7;)
+        {
+            if (tmp_lst.Contains(tmp_random))
+            {
+                tmp_random = Random.Range(0, 32);
+            }
+            else
+            {
+                tmp_lst.Add(tmp_random);
+                i++;
+            }
+        }
+
+
+    }
+
+
+
+
 }
--- Assets/Scripts/Select_card.cs
+++ Assets/Scripts/Select_card.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Select_card : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    GameObject card;
+
+    [SerializeField]
+    GameObject cardpos;
+    Rigidbody2D my_rigid;
+
+    Vector3 next_pos;
+
+    float speed = 3f;
+
+
+    Sprite[] my_sprite;
+    SpriteRenderer my_spriteRendere;
+
+
+
+    private void Awake()
+    {
+        my_rigid = transform.GetComponent<Rigidbody2D>();
+        my_spriteRendere = transform.GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
         
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void FixedUpdate()
+    {
+
+
+
+        //next_pos = cardpos[0].transform.position - transform.position;
+        //next_pos = next_pos.normalized;
+
+        //my_rigid.MovePosition(transform.position + next_pos * 5 * Time.fixedDeltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, cardpos[0].transform.position, 5 * Time.fixedDeltaTime);
+        transform.position = Vector3.Lerp(transform.position, cardpos.transform.position, speed * Time.fixedDeltaTime);
+        //transform.position = Vector3.Slerp(transform.position, cardpos[0].transform.position, 3 * Time.fixedDeltaTime);
+
+
+    }
+
+
+
+
+
+
+
+
 }
